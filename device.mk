1d0da3f8c440737fe27f92b4249d42d447f791b2
Subodh kumar <79571881+Subodhkr360@users.noreply.github.com>
2026-01-24 21:08:17 +0530

    Add OTA partitions to device.mk

--- device.mk
+++ device.mk
@@ -20,6 +20,19 @@ AB_OTA_PARTITIONS += \
     system_dlkm \
     vendor_dlkm
 
+AB_OTA_PARTITIONS += \
+    my_bigball \
+    my_carrier \
+    my_company \
+    my_engineering \
+    my_heytap \
+    my_manifest \
+    my_preload \
+    my_product \
+    my_region \
+    my_stock
+
+
 
 # Fastboot
 PRODUCT_PACKAGES += \
